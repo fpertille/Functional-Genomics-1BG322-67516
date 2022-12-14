fa3915e078a5b901de8a01419651321950d69c60
Violeta de Anca Prado <101873673+Violeta-de-Anca@users.noreply.github.com>
2022-12-14 07:12:51 +0100

    Update and rename from_alignment_to_count_matrix.sh to count_matrix_creation.sh
    As it is upload another script with the gff creation file in here it is only left the featureCounts part

---
+++ count_matrix_creation.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+##################################################################################
+######################### The count matrix is created ############################
+##################################################################################
+
+# FeatureCounts recognices the windows in the BAM file and produces a count matrix, this count matrix is going to be later used to do the statistic to see the differential methylated windows
+featureCounts -t sequence_feature -g gene_id -p -B -a /<path_GFF_file>/<GFF_file> -o /<path_output>/counts.<output>.txt /<path_input>/*.bam
+
