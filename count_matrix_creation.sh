6c53a383c073a6fceebdf0e9f119b6c7f923d4c4
Violeta de Anca Prado <101873673+Violeta-de-Anca@users.noreply.github.com>
2022-12-14 09:03:29 +0100

    Update count_matrix_creation.sh

--- count_matrix_creation.sh
+++ count_matrix_creation.sh
@@ -4,6 +4,7 @@
 ######################### The count matrix is created ############################
 ##################################################################################
 
+module load subread
 # FeatureCounts recognices the windows in the BAM file and produces a count matrix, this count matrix is going to be later used to do the statistic to see the differential methylated windows
 featureCounts -t sequence_feature -g gene_id -p -B -a /<path_GFF_file>/<GFF_file> -o /<path_output>/counts.<output>.txt /<path_input>/*.bam
 
