b1c725c4a1e5944a55b77d89484c068e0837b524
Violeta de Anca Prado <101873673+Violeta-de-Anca@users.noreply.github.com>
2022-12-15 09:20:06 +0100

    Update count_matrix_creation.sh
    added paths as variables and headers

--- count_matrix_creation.sh
+++ count_matrix_creation.sh
@@ -3,8 +3,18 @@
 ##################################################################################
 ######################### The count matrix is created ############################
 ##################################################################################
+#path to gff file
+i=path_to_gff_file
+# path to output folder
+j=path_to_output
+#path to the input folder (BAM files)
+k=path_to_input
+# name of the reference genome to make it unique
+b=reference_genome_name (like mm10)
 
+module load bioinfo-tools
 module load subread
+
 # FeatureCounts recognices the windows in the BAM file and produces a count matrix, this count matrix is going to be later used to do the statistic to see the differential methylated windows
-featureCounts -t sequence_feature -g gene_id -p -B -a /<path_GFF_file>/<GFF_file> -o /<path_output>/counts.<output>.txt /<path_input>/*.bam
+featureCounts -t sequence_feature -g gene_id -p -B -a $i/mm10.curated.gff -o $j/counts.$b.txt $k/*.bam
 
