c5479bc4d78390b94e090d6d0a64f5747544f99c
Fábio Pértille <43642965+fpertille@users.noreply.github.com>
2023-02-20 10:52:38 +0100

    Update 05.2. FragmentLengthRange.R

--- 05.2. FragmentLengthRange.R
+++ 05.2. FragmentLengthRange.R
@@ -10,6 +10,10 @@ template.length=read.table("C:/Users/viode560/Documents/statistics_work/template
 # visualization in the form of an histogram
 hist(abs(template.length$V1), main = "Template length hypothalamus", xlab = "Base pairs")
 
+#Assuming template.length is a data frame, the template.length$V1 syntax extracts the V1 column from the template.length data frame. 
+#The abs() function takes the absolute value of each element in the resulting column. 
+#The hist() function then creates a histogram of the absolute values of V1, with the specified title and x-axis label.
+
 #summary to have the minimum and maximum window length
 stats.HPT=summary(abs(template.length$V1))
 stats.HPT
