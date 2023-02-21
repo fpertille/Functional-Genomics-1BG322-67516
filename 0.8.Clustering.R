2db97584a52326dbcd88c745c348ef547210324c
Violeta de Anca Prado <101873673+Violeta-de-Anca@users.noreply.github.com>
2023-02-21 18:11:13 +0100

    Update 0.8.Clustering.R
    adding the code to have the coordinates as first column and the cluster column

--- 0.8.Clustering.R
+++ 0.8.Clustering.R
@@ -1,4 +1,4 @@
-#clusterization ####
+#clusterization using the k-means method, and the elbow method to know the optimal number of clusters ####
 setwd("C:/Users/your_user/")
 #install.packages("reshape")
 #install.packages("kohonen")
@@ -41,3 +41,18 @@ kcluster.3=kmeans(count.scaled.table.RBC,centers = 3, nstart = 100)
 k.means.cluster.metadata.RBC=kcluster.3
 save(k.means.cluster.metadata.RBC,file = "C:/Users/your_user/k.means.cluster.metadata.RBC.rda")
 graph.cluster=fviz_cluster(kcluster.3, data = count.scaled.table.RBC)
+
+# Now add the cluster to the original raw count matrix #####
+load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/count.table.final.counts.RBC.rda")
+count.table.RBC=cbind(as.data.frame(row.names(count.table.RBC)),count.table.RBC)
+names(count.table.RBC)[names(count.table.RBC) == "row.names(count.table.RBC)"] <- 'gene'
+count.table.raw.RBC=left_join(cluster.rbc,count.table.RBC,by="gene")
+save(count.table.raw.RBC,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/count.table.final.counts.RBC.rda")
+
+# Adding a cluster columm to the normalized count matrix #####
+load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/counts.final.cpm.tmm.normalized.RBC.rda")
+gene=as.data.frame(row.names(counts.final.cpm.tmm.normalized.RBC))
+counts.final.cpm.tmm.normalized.RBC=cbind(as.data.frame(row.names(counts.final.cpm.tmm.normalized.RBC)),counts.final.cpm.tmm.normalized.RBC)
+names(counts.final.cpm.tmm.normalized.RBC)[names(counts.final.cpm.tmm.normalized.RBC) == "row.names(counts.final.cpm.tmm.normalized.RBC))"] <- 'gene'
+counts.final.cpm.tmm.normalized.RBC=left_join(cluster.rbc,counts.final.cpm.tmm.normalized.RBC,by="gene")
+save(counts.final.cpm.tmm.normalized.RBC,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/counts.final.cpm.tmm.normalized.RBC.rda")
