6382b87d42470c139abd436ed80ecee7af7ddc13
Violeta de Anca Prado <101873673+Violeta-de-Anca@users.noreply.github.com>
2023-02-22 19:18:52 +0100

    Update 0.8.Clustering.R
    fixed bugs

--- 0.8.Clustering.R
+++ 0.8.Clustering.R
@@ -1,9 +1,4 @@
 #clusterization using the k-means method, and the elbow method to know the optimal number of clusters ####
-setwd("C:/Users/your_user/")
-#install.packages("reshape")
-#install.packages("kohonen")
-#install.packages("RColorBrewer")
-#install.packages("factoextra")
 library(writexl)
 library(plyr)
 library(tidyverse)
@@ -21,15 +16,13 @@ library(reshape)
 library(kohonen)
 library(RColorBrewer)
 library(factoextra)
-
-load("C:/Users/your_user/counts.final.cpm.tmm.normalized.RBC.rda")
-row.names(counts.final.cpm.tmm.normalized.RBC)=NULL
-count.scaled.table.RBC=counts.final.cpm.tmm.normalized.RBC
-count.scaled.table.RBC=scale(counts.final.cpm.tmm.normalized.RBC)
-
-# Heatmap visualizing all windows distance against all windows
-distance.rbc=get_dist(count.scaled.table.RBC)
-fviz_dist(distance.rbc, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+setwd("C:/Users/viode560/Documents/GBSMeDIP_workpipeline")
+load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/counts.final.cpm.tmm.normalized.RBC.rda")
+count.scaled.table.RBC=scale(counts.final.cpm.tmm.normalized.RBC[,2:159])
+row.names(count.scaled.table.RBC)=counts.final.cpm.tmm.normalized.RBC[,1]
+# Heatmap visualizing all windows distance against all windows, every computing demanding, will not run
+#distance.rbc=get_dist(count.scaled.table.RBC)
+#fviz_dist(distance.rbc, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
 
 # Perform the "elbow method" or sum of squares error (SSE) to have the number of clusters
 wss <- (nrow(count.scaled.table.RBC)-1)*sum(apply(count.scaled.table.RBC,2,var))
@@ -37,22 +30,19 @@ for (i in 2:10) wss[i] <- sum(kmeans(count.scaled.table.RBC,centers=i)$withinss)
 plot(1:10, wss, type="b", xlab="Number of Clusters",ylab="Within groups sum of squares")
 
 # Perform k-means clusterization method on our set of data
-kcluster.3=kmeans(count.scaled.table.RBC,centers = 3, nstart = 100)
-k.means.cluster.metadata.RBC=kcluster.3
-save(k.means.cluster.metadata.RBC,file = "C:/Users/your_user/k.means.cluster.metadata.RBC.rda")
-graph.cluster=fviz_cluster(kcluster.3, data = count.scaled.table.RBC)
+k.means.cluster.metadata.RBC=kmeans(count.scaled.table.RBC,centers = 3, nstart = 100)
+save(k.means.cluster.metadata.RBC,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/clusterization/k.means.cluster.metadata.RBC.rda")
+graph.cluster=fviz_cluster(k.means.cluster.metadata.RBC, data = count.scaled.table.RBC)
 
 # Now add the cluster to the original raw count matrix #####
 load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/count.table.final.counts.RBC.rda")
-count.table.RBC=cbind(as.data.frame(row.names(count.table.RBC)),count.table.RBC)
-names(count.table.RBC)[names(count.table.RBC) == "row.names(count.table.RBC)"] <- 'gene'
-count.table.raw.RBC=left_join(k.means.cluster.metadata.RBC,count.table.RBC,by="gene")
+k.means.cluster.metadata.RBC=as.data.frame(k.means.cluster.metadata.RBC$cluster)
+k.means.cluster.metadata.RBC=cbind(as.data.frame(rownames(k.means.cluster.metadata.RBC)),k.means.cluster.metadata.RBC)
+colnames(k.means.cluster.metadata.RBC)=c("location","cluster")
+count.table.raw.RBC=left_join(k.means.cluster.metadata.RBC,count.table.RBC,by="location")
 save(count.table.raw.RBC,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/count.table.final.counts.RBC.rda")
 
 # Adding a cluster columm to the normalized count matrix #####
 load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/counts.final.cpm.tmm.normalized.RBC.rda")
-gene=as.data.frame(row.names(counts.final.cpm.tmm.normalized.RBC))
-counts.final.cpm.tmm.normalized.RBC=cbind(as.data.frame(row.names(counts.final.cpm.tmm.normalized.RBC)),counts.final.cpm.tmm.normalized.RBC)
-names(counts.final.cpm.tmm.normalized.RBC)[names(counts.final.cpm.tmm.normalized.RBC) == "row.names(counts.final.cpm.tmm.normalized.RBC))"] <- 'gene'
-counts.final.cpm.tmm.normalized.RBC=left_join(k.means.cluster.metadata.RBC,counts.final.cpm.tmm.normalized.RBC,by="gene")
+counts.final.cpm.tmm.normalized.RBC=left_join(k.means.cluster.metadata.RBC,counts.final.cpm.tmm.normalized.RBC,by="location")
 save(counts.final.cpm.tmm.normalized.RBC,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/counts.final.cpm.tmm.normalized.RBC.rda")
