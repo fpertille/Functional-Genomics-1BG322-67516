896cf31a2befde265ec1815858e565a10f39ef82
Fábio Pértille <43642965+fpertille@users.noreply.github.com>
2023-02-22 11:35:22 +0100

    Update 0.7.PreparingDataAndNormalizing.R

--- 0.7.PreparingDataAndNormalizing.R
+++ 0.7.PreparingDataAndNormalizing.R
@@ -13,6 +13,9 @@ setwd("C:/Users/your_user/")
 count.table.RBC = fread(file= paste0(getwd(),"","/count_matrix/counts.matrix.RBC.galgal7.txt"))
 
 # We are going to filter for the maximum and minimum template length of the library, script [05.2.FragmentLengthRange.R]
+
+#ideally we want to load(stats) from script 05.2 and add a command to the next two line to automatically take these two values
+#we take the maximum and minum from stats 
 count.table.RBC=count.table.RBC%>%filter(Length<<maximum_length>)
 count.table.RBC=count.table.RBC%>%filter(Length><minimum_length>)
 # Do some formatting of the matrix
