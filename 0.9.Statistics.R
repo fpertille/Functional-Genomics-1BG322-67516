92f0e7e9639f4bdc8afb4443518ae41a3a3ee18f
Violeta de Anca Prado <101873673+Violeta-de-Anca@users.noreply.github.com>
2023-02-23 10:53:28 +0100

    Update 0.9.Statistics.R
    final version with the fold change included in the master table

--- 0.9.Statistics.R
+++ 0.9.Statistics.R
@@ -11,7 +11,7 @@ library(dplyr)
 
 # Mann-whitney ####
 load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/design.list")
-load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/counts.final.cpm.tmm.normalized.RBC")
+load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/counts.final.cpm.tmm.normalized.RBC.rda")
 group.list.RBC=as.data.frame(design.list$blindtreatment)
 colnames(group.list.RBC)=c("group")
 list.mann.whitney.RBC=list()
@@ -19,7 +19,7 @@ for (i in 1:nrow(counts.final.cpm.tmm.normalized.RBC)) {
   counts.window.RBC=as.data.frame(t(counts.final.cpm.tmm.normalized.RBC[i,-(1:2)]))
   colnames(counts.window.RBC)=c("V1")
   out = wilcox.test(counts.window.RBC$V1~group.list.RBC$group)
-  out[8]=counts.final.cpm.tmm.normalized.RBC$gene[i]
+  out[8]=counts.final.cpm.tmm.normalized.RBC$location[i]
   list.mann.whitney.RBC[[i]]=out
 }
 
@@ -28,9 +28,9 @@ df.RBC=as.data.frame(do.call(cbind, list.mann.whitney.RBC))
 df.RBC=t(df.RBC)
 df.RBC=as.data.frame(df.RBC)
 p.values.RBC=df.RBC[,c(3,8)]
-colnames(p.values.RBC)=c("p_value","window")
+colnames(p.values.RBC)=c("p_value","location")
 p.values.mann.whitney=as.data.frame(t(rbindlist(lapply(p.values.RBC, as.data.table))))
-colnames(p.values.mann.whitney)=c("p_value","gene")
+colnames(p.values.mann.whitney)=c("p_value","location")
 save(p.values.mann.whitney,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/p.values.mann.whitney.rda")
 
 # Perform the Benjamini-Hochberg correction for multiple testing #####
@@ -38,7 +38,7 @@ p.values.mann.whitney$corrected_p_value = p.adjust(((p.values.mann.whitney$p_val
 save(p.values.mann.whitney,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/p.values.mann.whitney.rda")
 
 # Moderated t test #####
-load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/count.table.raw.RBC")
+c=load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/count.table.final.counts.RBC.rda")
 load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/design.list")
 counts.raw.rbc=count.table.raw.RBC
 dge <- DGEList(counts=counts.raw.rbc[,-c(1:2)], genes=counts.raw.rbc[,1])
@@ -52,20 +52,18 @@ top.p.values.t.test.limma=topTable(fit, coef=ncol(design),number = nrow(counts.r
 save(top.p.values.t.test.limma,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/t.test.limma.rbc.rda")
 
 # Perform the Benjamini-Hochberg correction for multiple testing #####
-top.p.values.t.test.limma$corrected_p_value = p.adjust((unlist(top.p.values.t.test.limma$P.Value)), method = "BH")
+top.p.values.t.test.limma$corrected_p_value = p.adjust(((top.p.values.t.test.limma$P.Value)), method = "BH")
 save(top.p.values.t.test.limma,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/top.p.values.t.test.limma.rda")
 
+load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/top.p.values.t.test.limma.rda")
+load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/p.values.mann.whitney.rda")
 # Create master tables ####
-top.p.values.t.test.limma$gene=row.names(top.p.values.t.test.limma)
-top.p.values.t.test.limma=left_join(top.p.values.t.test.limma,count.table.raw.RBC, by="gene")
-master.table.moderated.t.test=top.p.values.t.test.limma[,c(8,9,4,7)]
-names(master.table.moderated.t.test)[names(master.table.moderated.t.test) == "gene"] <- 'location'
+top.p.values.t.test.limma$location=row.names(top.p.values.t.test.limma)
+top.p.values.t.test.limma=left_join(top.p.values.t.test.limma,count.table.raw.RBC, by="location")
+master.table.moderated.t.test=top.p.values.t.test.limma[,c(8,9,4,7,1)]
 save(master.table.moderated.t.test,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/master.table.moderated.t.test.rda")
 
-load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/p.values.mann.whitney.rda")
-p.values.mann.whitney=left_join(p.values.mann.whitney,count.table.raw.RBC, by="gene")
+load("C:/Users/viode560/Documents/GBSMeDIP_workpipeline/count.table.final.counts.RBC.rda")
+p.values.mann.whitney=left_join(p.values.mann.whitney,count.table.raw.RBC, by="location")
 master.table.mann.whitney=p.values.mann.whitney[,c(2,4,1,3)]
-names(master.table.mann.whitney)[names(master.table.mann.whitney) == "gene"] <- 'location'
 save(master.table.mann.whitney,file = "C:/Users/viode560/Documents/GBSMeDIP_workpipeline/master.table.mann.whitney.rda")
-
-
